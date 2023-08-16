8a7f29d73642a14fe167ce369cf0191aa2e909a4
Jaerin Lee <jarin.lee@gmail.com>
2023-08-16 23:15:36 +0900

    edsr-baseline

--- src/run.sh
+++ src/run.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
 # SVR_NAME=NIPA
 SVR_NAME=Jupiter
-LABEL=abl_c5
+LABEL=abl_nobalanceloss
 
-MODEL=RRDB
+MODEL=EDSR
+DATA_ROOT='/mnt/ssd2/frontier/datasets'
 
 LRGB_ROOT='../../lrgb_pretrained'
-LRGB_NAME='ABLATION/div2k-class5'
+LRGB_NAME='ABLATION/div2k_nobalanceloss'
 LRGB_MODEL='model_latest.pt'
-LRGB_N_EXPERTS=5
+LRGB_N_EXPERTS=20
 
 LABEL=${MODEL}_${LABEL}
 
 LRGB_PATH=${LRGB_ROOT}/${LRGB_NAME}/model/${LRGB_MODEL}
 
-CUDA_VISIBLE_DEVICES=1,2 python main.py --save ${LABEL} --model ${MODEL} --n_GPUs 2 \
-    --lrgb_ckpt ${LRGB_PATH} --lrgb_n_experts ${LRGB_N_EXPERTS} --lrgb_use_legacy
+CUDA_VISIBLE_DEVICES=0,1 python main.py --dir_data ${DATA_ROOT} --save ${LABEL} --model ${MODEL} --n_GPUs 2 \
+    --lrgb_ckpt ${LRGB_PATH} --lrgb_n_experts ${LRGB_N_EXPERTS} # --lrgb_use_legacy
 
 
 SMS_MSG=${LABEL}' @ '${SVR_NAME}' is over.'
